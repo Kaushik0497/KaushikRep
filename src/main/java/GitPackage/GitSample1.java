811ffcefd0725ee07225040b187d062f74164c37
Kaushik0497 <152833780+Kaushik0497@users.noreply.github.com>
2025-01-11 14:32:15 +0530

    Update GitSample1.java

--- src/main/java/GitPackage/GitSample1.java
+++ src/main/java/GitPackage/GitSample1.java
@@ -5,5 +5,7 @@ public class GitSample1
 	public static void main(String[] args) 
 	{
 		System.out.println("---running GitSample1---");
+		System.out.println("---running GitSample2---");
+		System.out.println("---running GitSample3---");
 	}
 }
